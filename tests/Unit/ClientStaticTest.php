c795dc6e264a55d479a857992f6f8d04954d4ec6
ZGrguric <zgrguric@xrplwin.com>
2022-08-22 18:52:53 +0200

    Purposely fail tests

--- tests/Unit/ClientStaticTest.php
+++ tests/Unit/ClientStaticTest.php
@@ -9,6 +9,8 @@ final class ClientStaticTest extends TestCase
 {
     public function testSnakeToCaseFunction(): void
     {
+      $this->assertTrue(false);
+      
       $check1 = \XRPLWin\XRPL\Client::snakeToCase('func_name_one');
       $this->assertEquals($check1,'FuncNameOne');
 
