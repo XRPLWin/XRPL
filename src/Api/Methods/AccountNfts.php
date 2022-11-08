7eacb89e4d88b53f54baaf25a6a6b4a61b35d2b7
ZGrguric <zgrguric@xrplwin.com>
2022-11-08 19:22:49 +0100

    AccountNfts method

---
+++ src/Api/Methods/AccountNfts.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace XRPLWin\XRPL\Api\Methods;
+
+use XRPLWin\XRPL\Api\AbstractMethod;
+use XRPLWin\XRPL\Exceptions\NotSentException;
+use XRPLWin\XRPL\Exceptions\XRPL\NotSuccessException;
+
+class AccountNfts extends AbstractMethod
+{
+  protected string $method = 'account_nfts';
+  protected string $endpoint_config_key = 'endpoint_reporting_uri';
+
+  /**
+   * Returns current ledger index.
+   * @return stdClass
+   * @throws NotExecutedException
+   */
+  public function finalResult(): \stdClass
+  {
+    if(!$this->executed)
+      throw new NotSentException('Please send request first');
+
+    if(!$this->isSuccess())
+      throw new NotSuccessException('Request did not return success result: '.\json_encode($this->result));
+
+    return (object)$this->result()->result->account_nfts;
+  }
+}
